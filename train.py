e6620186686b723df9e21ecae500a631b1b98f30
DanaOsama <danaosama1999@gmail.com>
2024-02-26 21:17:13 +0400

    Added qualitative results (ribbon diagram and confusion matrix), as well as per phase logging of metrics

--- train.py
+++ train.py
@@ -9,15 +9,11 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataloaders import Cataracts_101_21_v2
 from utils import *
-import random
 import time
 
 # LOGGING
 import wandb
 
-import logging
-import sys
-from logging import StreamHandler, Formatter, FileHandler
 from prettytable import PrettyTable
 import argparse
 
@@ -44,6 +40,12 @@ parser.add_argument(
     default="/l/users/dana.mohamed/checkpoints/",
 )
 
+parser.add_argument(
+    "--qualitative_results_path",
+    type=str,
+    help="Path to save the qualitative results, including ribbon figure and confusion matrices",
+    default="qualitative_results/",
+)
 parser.add_argument(
     "--num_classes", type=int, help="Number of classes, default=10", default=10
 )
@@ -128,12 +130,7 @@ parser.add_argument(
     help="Whether to resume training from a checkpoint",
     default=False,
 )
-parser.add_argument(
-    "--random_int",
-    type=int,
-    help="Used when resuming training for a specific model",
-    default=0,
-)
+
 parser.add_argument(
     "--wandb_run_id",
     type=str,
@@ -167,6 +164,17 @@ rnn_model = args.rnn_model
 root_dir = args.root_dir
 json_path = args.json_path
 checkpoint_path = args.checkpoint_path
+qualitative_results_path = args.qualitative_results_path
+
+
+# Create a the directory to save the checkpoints
+if not os.path.exists(checkpoint_path):
+    os.makedirs(checkpoint_path)
+    print("[INFO] created checkpoints directory")
+
+if not os.path.exists(qualitative_results_path):
+    os.makedirs(qualitative_results_path)
+    print("[INFO] created qualitative results directory")
 
 # Data related args
 dataset = args.dataset
@@ -200,27 +208,30 @@ optimizer = (
     else None
 )
 # ####################################################################################################################################
+project_name = "Phase_detection"
+run_id = None
 
 # Add Wandb logging
 if args.resume_training and args.wandb_run_id is not None:
-    random_int = args.random_int
-    wandb_run_id = args.wandb_run_id
-    wandb.init(project="Thesis", id=wandb_run_id, resume="allow")
+    run_id = args.wandb_run_id
+    wandb.init(project=project_name, id=run_id, resume="allow")
 
-else:
-    random_int = random.randint(0, 1000000)
+else: # First time training
     exp_name = (
-        f"{architecture}___{cnn_model}_{rnn_model}_{num_clips}_{clip_size}_{random_int}"
+        f"{architecture}___{cnn_model}_{rnn_model}_{num_clips}_{clip_size}"
         if architecture == "CNN_RNN"
-        else f"{architecture}___{cnn_model}_{num_clips}_{clip_size}_{random_int}"
+        else f"{architecture}___{cnn_model}_{num_clips}_{clip_size}"
     )
     wandb.init(
         # set the wandb project where this run will be logged
-        project="Thesis",
+        project=project_name,
         name=exp_name,
         # track hyperparameters and run metadata
         config=args,
     )
+
+run_id = wandb.run.id
+print(f"[INFO] Run ID: {run_id}")
 
 # number of parameters in the model
 print(
@@ -237,12 +248,6 @@ transform = v2.Compose(
         # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Example normalization
     ]
 )
-
-
-# Create a the directory to save the checkpoints
-if not os.path.exists(checkpoint_path):
-    os.makedirs(checkpoint_path)
-    print("[INFO] created checkpoints directory")
 
 train_dataset = Cataracts_101_21_v2(
     root_dir,
@@ -291,7 +296,7 @@ if num_clips == -1:
 else:
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 best_val_acc = 0  # to keep track of best validation accuracy
 best_val_epoch = -1
@@ -303,7 +308,7 @@ start_time = time.time()
 if args.resume_training:
     # Load the model
     ckpt = load_checkpoint(
-        model, optimizer, checkpoint_path + f"last_epoch_{random_int}.pth"
+        model, optimizer, checkpoint_path + f"last_epoch_{run_id}.pth"
     )
 
     start_epoch = ckpt["epoch"]
@@ -333,7 +338,7 @@ for epoch in range(start_epoch, epochs):
         f"[INFO] Epoch {epoch+1}/{epochs}, train loss: {loss:.4f}, val accuracy: {acc:.4f}"
     )
     save_checkpoint(
-        model, optimizer, epoch, checkpoint_path + f"last_epoch_{random_int}.pth"
+        model, optimizer, epoch, checkpoint_path + f"last_epoch_{run_id}.pth"
     )  # save checkpoint after each epoch
     if acc > best_val_acc:
         best_val_acc = acc
@@ -342,7 +347,7 @@ for epoch in range(start_epoch, epochs):
             model,
             optimizer,
             epoch,
-            checkpoint_path + f"best_model_{random_int}.pth",
+            checkpoint_path + f"best_model_{run_id}.pth",
             best=True,
         )
         best_val_epoch = epoch
@@ -377,18 +382,19 @@ table.add_data("Accuracy", best_metrics["accuracy"])
 table.add_data("Precision", best_metrics["precision"])
 table.add_data("Recall", best_metrics["recall"])
 table.add_data("F1-Score", best_metrics["f1_score"])
-wandb.log({"results_table": table}, commit=False)
+wandb.log({"validation_results_table": table}, commit=False)
 
 
 # Test the model
 # load best_model
 ckpt = load_checkpoint(
-    model, optimizer, checkpoint_path + f"best_model_{random_int}.pth"
+    model, optimizer, checkpoint_path + f"best_model_{run_id}.pth"
 )
 model.load_state_dict(ckpt["model_state_dict"])
 
 test_metrics = validate(model, test_loader, DEVICE)
 
+# Log the test results on Wandb
 table = wandb.Table(columns=["Metrics_Test_Set", "Value"])
 table.add_data("Accuracy", test_metrics["accuracy"])
 table.add_data("Precision", test_metrics["precision"])
@@ -396,6 +402,15 @@ table.add_data("Recall", test_metrics["recall"])
 table.add_data("F1-Score", test_metrics["f1_score"])
 wandb.log({"test_results_table": table}, commit=False)
 
+columns_per_class = ["Metric"] + [f"Phase_{i}" for i in range(num_classes)]
+table = wandb.Table(columns=columns_per_class)
+table.add_data(*(["Jaccard"] + [str(j) for j in test_metrics["jaccard_per_class"]]))
+table.add_data(*(["Precision"] + [str(p) for p in test_metrics["precision_per_class"]]))
+table.add_data(*(["Recall"] + [str(r) for r in test_metrics["recall_per_class"]]))
+table.add_data(*(["F1-Score"] + [str(f) for f in test_metrics["f1_per_class"]]))
+wandb.log({"test_results_table_per_class": table}, commit=False)
+
+# Print the results
 # Create a table with the results
 print("TEST SET RESULTS:")
 print("#################")
@@ -409,12 +424,33 @@ print(results)
 
 print("Run ID: ", wandb.run.id)
 print("Run URL: ", wandb.run.get_url())
-print(f"Random int: {random_int}")
 
 wandb.run.summary["test_accuracy"] = test_metrics["accuracy"]
 wandb.run.summary["test_precision"] = test_metrics["precision"]
 wandb.run.summary["test_recall"] = test_metrics["recall"]
 wandb.run.summary["test_f1_score"] = test_metrics["f1_score"]
 
+# Save confusion matrix
+create_confusion_matrix = True
+if create_confusion_matrix:
+    cf_matrix = test_metrics["confusion_matrix"]
+    confusion_path = os.path.join(qualitative_results_path, f"confusion_matrix_{run_id}.png")
+    
+    save_confusion_matrix(cf_matrix, confusion_path)
+    wandb.log({"confusion_matrix": wandb.Image(confusion_path)})
+
+# Save qualitative results
+create_qualitative_results = True
+if create_qualitative_results:
+    qualitative_results_path = os.path.join(qualitative_results_path, f"ribbon_diagram_{run_id}.png")
+    batch = next(iter(test_loader))
+    make_qualitative_results(
+        {f"{architecture}": model},
+        next(iter(test_loader)),
+        qualitative_results_path,
+        num_classes,
+        DEVICE,
+    )
+    wandb.log({"qualitative_results": wandb.Image(qualitative_results_path)})
 
 wandb.finish()
--- utils.py
+++ utils.py
@@ -10,8 +10,15 @@ from sklearn.metrics import (
     precision_score,
     f1_score,
     accuracy_score,
+    jaccard_score,
+    confusion_matrix
 )
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.colors import ListedColormap
+import matplotlib as mpl
 
 def save_checkpoint(model, optimizer, epoch, path, best=False):
     checkpoint = {
@@ -50,7 +57,6 @@ def train(model, optimizer, criterion, train_loader, DEVICE):
         labels = torch.argmax(labels, dim=-1)
         labels = labels.view(-1)
         total_frames += num_frames*batch_size
-        print("Total_frames:", total_frames)
 
         # Expected input shape: (batch_size * num_frames, num_classes)
         # Expected label shape: (batch_size * num_frames) where each label is a class index
@@ -74,11 +80,9 @@ def validate(model, validation_loader, DEVICE):
     with torch.no_grad():  # No need to track gradients during validation
         for data, labels in tqdm(validation_loader):
             data, labels = data.to(DEVICE), labels.to(DEVICE)
-
             # Forward pass: compute the model output
             outputs = model(data)
             _, predicted_indices = torch.max(outputs, 2)
-
             all_predicted.extend(predicted_indices.cpu().numpy())
 
             # Convert one-hot encoded labels to class indices for comparison
@@ -95,12 +99,124 @@ def validate(model, validation_loader, DEVICE):
     recall = recall_score(all_labels, all_predicted, zero_division=0, average="macro")
     f1 = f1_score(all_labels, all_predicted, zero_division=0, average="macro")
     accuracy = accuracy_score(all_labels, all_predicted)
+    jaccard = jaccard_score(all_labels, all_predicted, average="macro")
+    cf_matrix = confusion_matrix(all_labels, all_predicted)
+    
+    # Per class metrics, where the averaging is set to None.
+    precision_per_class = precision_score(all_labels, all_predicted, zero_division=0, average=None)
+    recall_per_class = recall_score(all_labels, all_predicted, zero_division=0, average=None)
+    f1_per_class = f1_score(all_labels, all_predicted, zero_division=0, average=None)
+    jaccard_per_class = jaccard_score(all_labels, all_predicted, average=None)
 
     metrics = {
         "accuracy": accuracy,
         "precision": precision,
         "recall": recall,
         "f1_score": f1,
+        "jaccard": jaccard,
+        "precision_per_class": precision_per_class,
+        "recall_per_class": recall_per_class,
+        "f1_per_class": f1_per_class,
+        "jaccard_per_class": jaccard_per_class,
+        "confusion_matrix": cf_matrix,
     }
 
     return metrics
+
+def save_confusion_matrix(cf_matrix, path):
+    plt.figure(figsize=(8, 6))  # Set the figure size
+    sns.heatmap(cf_matrix, annot=True)
+    # Add labels and title
+    plt.ylabel('Ground Truth')
+    plt.xlabel('Predictions')
+    plt.title('Confusion Matrix')
+
+    # Add tick labels (e.g., class names)
+    num_classes = cf_matrix.shape[0]
+    class_names = [f"P{i}" for i in range(num_classes)]
+
+    plt.xticks(np.arange(len(class_names)) + 0.5, class_names, rotation=45)
+    plt.yticks(np.arange(len(class_names)) + 0.5, class_names, rotation=45)
+    plt.savefig(path, bbox_inches='tight', dpi=300)
+
+def plot_results(predictions, title, position, total_plots, cmap=None):
+    """
+    plot_results: Function to plot the results of the predictions
+    :param predictions: Predictions (np.array) (num_frames,)
+    :param title: Title of the plot (str)
+    :param position: Position of the plot (int)
+    :param total_plots: Total number of plots (int)
+    """
+    plt.subplot(total_plots, 1, position)
+    plt.imshow(predictions[np.newaxis, :], cmap=cmap, aspect="auto")
+    plt.gca().set_yticks([])
+    plt.gca().set_xticks(np.arange(0, len(predictions), 10))
+    plt.ylabel(title)
+
+def make_qualitative_results(models_dic, batch, path, num_classes, DEVICE):
+    """
+    Function to plot the qualitative results of the predictions
+    :param models_dic: Dictionary of models (dict) {"model_name": model}
+    :param batch: Batch of data (tuple) (inputs, labels)
+        inputs: (batch_size, num_frames, channels, height, width)
+        labels: (batch_size, num_frames, num_classes)
+        batch_size should be 1
+    :param path: Path to save the plot (str)
+    :param num_classes: Number of classes (int)
+    :param DEVICE: Device to run the models (str)
+    """
+    num_of_models = len(models_dic)
+    if num_classes > 10:
+        custom_colors = ['#1f77b4','#aec7e8','#ff7f0e','#ffbb78','#2ca02c','#98df8a',
+                         '#d62728','#ff9896','#9467bd','#c5b0d5','#8c564b','#c49c94',
+                         '#e377c2','#f7b6d2','#7f7f7f','#c7c7c7','#bcbd22','#dbdb8d',
+                         '#17becf','#9edae5']
+
+    else:
+        custom_colors = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b',
+                    '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2']
+    
+    cmap = ListedColormap(custom_colors)
+
+    # Load the data
+    inputs, labels = batch
+    if inputs.shape[0] != 1:
+        print("[INFO] Batch size is greater than 1, taking the first sample only")
+        inputs = inputs[0][np.newaxis, ...]
+        labels = labels[0][np.newaxis, ...]
+
+    inputs = inputs.to(DEVICE)
+    labels = labels.to(DEVICE)
+
+    # Get the ground truth
+    ground_truth = torch.argmax(labels, dim=-1).squeeze().cpu().numpy() # (batch_size, num_frames) if batch_size is not 1, else (num_frames,)
+
+    # Create a dictionary to store the predictions
+    predictions = {}
+    
+    for model in models_dic.keys():
+        models_dic[model].eval()
+        models_dic[model] = models_dic[model].to(DEVICE)
+        model_predictions = models_dic[model](inputs).cpu().detach().numpy()
+        predictions[model] = np.argmax(model_predictions, axis=-1).squeeze() # (batch_size, num_frames) if batch_size is not 1, else (num_frames,)
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+
+    # First, plot the ground truth
+    plot_results(ground_truth, "Ground Truth", 1, num_of_models, cmap)
+
+    # Plot the predictions
+    for i, model_name in enumerate(models_dic.keys()):
+        # def plot_results(predictions, title, position, total_plots, cmap=None):
+        plot_results(predictions[model_name], model_name, i+2, num_of_models + 1, cmap)
+
+    # Create a legend
+    handles = [mpatches.Patch(color=custom_colors[i], label=f'Phase {i+1}') for i in range(len(custom_colors))]
+    plt.legend(handles=handles, bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    # Final adjustments
+    plt.xticks(np.arange(0, len(ground_truth), 10), np.arange(0, len(ground_truth), 10))
+    plt.xlabel("Frame Number")
+    plt.tight_layout()
+    plt.savefig(path, bbox_inches='tight', dpi=300)
