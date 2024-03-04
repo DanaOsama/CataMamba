72075f6199c5ee533b04d61f8f36e086c20fa5a2
DanaOsama <dana.os.mohamed@gmail.com>
2024-03-04 23:31:32 +0400

    Most recent updates

--- scripts/run_all_ViT.sh
+++ scripts/run_all_ViT.sh
@@ -1,1 +1,18 @@
-python train.py --run_name learning_rate_5e-3 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+
+
+python train.py --run_name test --seed 0 --architecture ViT --epochs 1 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+
+
+python train.py --run_name 101-seed0 --seed 0 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+python train.py --run_name 101-seed1 --seed 1 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+python train.py --run_name 101-seed2 --seed 2 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+
+python train.py --run_name CATARACTS-seed0 --seed 0 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False --dataset 9_CATARACTS --num_classes 18
+python train.py --run_name CATARACTS-seed1 --seed 1 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False --dataset 9_CATARACTS --num_classes 18
+python train.py --run_name CATARACTS-seed2 --seed 2 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False --dataset 9_CATARACTS --num_classes 18
+
+python train.py --run_name 101-seed3 --seed 3 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+python train.py --run_name 101-seed4 --seed 4 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False
+
+python train.py --run_name CATARACTS-seed3 --seed 3 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False --dataset 9_CATARACTS --num_classes 18
+python train.py --run_name CATARACTS-seed4 --seed 4 --architecture ViT --epochs 50 --optimizer AdamW --num_clips 8 --clip_size 18 --step_size 2 --weight_decay 0.3 --learning_rate 5e-5 --label_smoothing 0.11 --scheduler Cosine --clip-grad-norm False --dataset 9_CATARACTS --num_classes 18
